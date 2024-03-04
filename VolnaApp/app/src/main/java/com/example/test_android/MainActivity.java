e1ef6c1e601629b7d7df5c811342d3a67c00acd7
afatin <artemfatin08@gmail.com>
2024-03-04 21:28:28 +0400

    Основа приложения

---
+++ VolnaApp/app/src/main/java/com/example/test_android/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.test_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void to_cinema(View viev){
+        Intent intent = new Intent(this, cinema.class);
+        startActivity(intent);
+    }
+
+}
